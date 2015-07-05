bfcc62be100f5369d4238635b48f4fb1f9a5f957
Jason Quaccia <jlquaccia@gmail.com>
2015-07-05 15:26:20 -0700

    Assignment 32 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,57 @@
+# require 'faker'
+
+# # Create Posts
+# 50.times do
+#   Post.create!(
+#     title: Faker::Lorem.sentence,
+#     body: Faker::Lorem.paragraph
+#   )
+# end
+
+# posts = Post.all
+
+# # Create Comments
+
+# 100.times do
+#   Comment.create!(
+#     post: posts.sample,
+#     body: Faker::Lorem.paragraph
+#   )
+# end
+
+# puts "Seed finished"
+# puts "#{Post.count} posts created"
+# puts "#{Comment.count} comments created"
+
+# Assignment 32 /////////////////////////////
+
 require 'faker'
 
-# Create Posts
-50.times do
+# Create Post
+1.times do
   Post.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
+    title: Faker::Hacker.abbreviation,
+    body: Faker::Hacker.say_something_smart
   )
 end
 
-posts = Post.all
+last_post = Post.last
 
-# Create Comments
-
-100.times do
+# Create Comment
+1.times do
   Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
+    post: last_post,
+    body: Faker::Team.creature
   )
 end
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
+# Post.create!(
+#   title: "Test Post Testing",
+#   body: "Blah blah blah"
+# )
+
+# ///////////////////////////////////////////
