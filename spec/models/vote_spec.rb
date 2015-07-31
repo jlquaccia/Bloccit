ef22375a209d24573023021809a3201acee3ad46
Jason Quaccia <jlquaccia@gmail.com>
2015-07-31 09:29:40 -0700

    Assignment 48 completed, Intro to RSpec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,12 +1,17 @@
+require 'rails_helper'
+
 describe Vote do
   describe "validations" do
     describe "value validation" do
-      before do
-        @post = Post.create(title: 'post title', body: 'post body')
-      end
-
       it "only allows -1 or 1 as values" do
-        expect(@post.value).to eq(1 || -1)
+        v1 = Vote.new(value: 1)
+        expect(v1.valid?).to eq(true)
+
+        v2 = Vote.new(value: -1)
+        expect(v2.valid?).to eq(true)
+
+        v3 = Vote.new(value: 2)
+        expect(v3.valid?).to eq(false)
       end
     end
   end
