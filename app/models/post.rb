9246363a6bddf9696fb4990d717d00e7da606216
Jason Quaccia <jlquaccia@gmail.com>
2015-08-01 13:30:23 -0700

    Checkpoint 50 completed, Mocking with RSpec

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,8 +22,8 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  # validates :topic, presence: true
-  # validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
   def update_rank
     age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
@@ -31,10 +31,6 @@ class Post < ActiveRecord::Base
 
     update_attribute(:rank, new_rank)
   end
-
-  after_create :create_vote
-
-  private
 
   def create_vote
     user.votes.create(value: 1, post: self)
