2bae43725ebba71746db08637958c88ade04f3d1
Jason Quaccia <jlquaccia@gmail.com>
2015-08-07 22:34:24 -0700

    Assignment 56 completed, Testing Users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,13 +15,10 @@ describe User do
 
   describe ".top_rated" do
     before do
-      @user1 = create(:user)
-      post = create(:post, user: @user1)
-      create(:comment, user: @user1, post: post)
-
-      @user2 = create(:user)
-      post = create(:post, user: @user2)
-      2.times { create(:comment, user: @user2, post: post) }
+      @user1 = create(:user_with_post_and_comment)
+      @user2 = create(:user_with_post_and_comment)
+      post = @user2.posts.first
+      create(:comment, post: post, user: @user2)
     end
 
     it "returns users ordered by comments + posts" do
